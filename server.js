5e20b4747d8f956becfe3cab9cbe46353587b133
Matt Paul <mattpaul001@icloud.com>
2016-01-04 21:29:09 +0000

    Calculate upload status

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var formidable = require('formidable')
 
 var server = http.createServer(function(req,res) {
   switch (req.method) {
@@ -22,9 +23,43 @@ function show(req, res) {
 res.setHeader('Content-Type', text/html);
 res.setHeader('Content-Length', Buffer.byteLength(html));
 res.end(html);
-
 };
 
 function upload(req,res) {
-  //upload logic
+  if (!isFormData(req)) {
+    res.statusCode = 400;
+    res.end('Bad Request: expecting multipart/form-data');
+    return;
+  }
+  var form = new formidable.IncomingForm();
+
+  form.on('field', function(field, value) {
+    console.log(field);
+    console.log(value);
+  });
+
+  form.on('file', function(name, file) {
+    console.log('name');
+    console.log('file');
+  });
+
+  form.on('end', function() {
+    res.end('upload complete!');
+  });
+
+  form.parse(req);
+
+
+//calculate upload progress
+form.on('progress', function(bytesReceived, bytesExpected) {
+  var percent = Math.floor(bytesReceived/bytesExpected * 100);
+  console.log(percent);
+  });
 }
+// helper method for the upload
+function isFormData(req) {
+  var type = req.headers['content-type'] || '';
+  return 0 == type.indexOf('multipart/form-data');
+}
+
+
